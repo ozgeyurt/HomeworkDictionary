0c3a54b8d7a18c1e3a5706ce13212e151c81aa18
ozgeyurt <ozge.yurt@windowslive.com>
2021-01-30 20:48:17 +0300

    Add project files.

---
+++ HomeworkDictionary/MyDictionary.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeworkDictionary
+{
+    class MyDictionary<K,V>
+    {
+        K[] keys;
+        V[] values;
+
+        public MyDictionary()   //program cs de new yaptığımız an burası çalışıyor
+        {
+            keys = new K[0];    //dizinin eleman sayısını sıfırlıyoruz 
+            values = new V[0];  
+        }
+        public void Add(K key,V value)
+        {
+            K[] tempKeys = keys;
+            V[] tempValue = values; //newleme yaptığımız an eski elemanlar silineceği için onları(referans tutucu) burada tutturuyoruz
+            keys = new K[keys.Length + 1];
+            values = new V[values.Length + 1];//eleman sayısını 1 arttırcaz
+
+            for (int i = 0; i < tempKeys.Length ; i++)
+            {
+                keys[i] = tempKeys[i];
+            }
+            
+            for (int j = 0; j < tempValue.Length; j++)
+            {
+                values[j] = tempValue[j];
+            }
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+
+            
+        }
+        
+    }
+}
---
+++ HomeworkDictionary/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HomeworkDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> dictionary = new Dictionary<int, string>();
+            dictionary.Add(312,"Ankara") ;
+            dictionary.Add(212, "İstanbul Avr");
+            dictionary.Add(216, "İstanbul And");
+            dictionary.Add(232, "İzmir");
+            dictionary.Add(374, "Bolu");
+            dictionary.Add(222, "Eskişehir");
+            dictionary.Add(378, "Bartın");
+
+            Console.WriteLine("Count : " + dictionary.Count);
+
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine(item);
+                
+            }
+        }
+
+    }
+}
